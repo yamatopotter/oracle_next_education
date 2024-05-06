cbbf446b8f7995ca54a736630c3c2b08d3eec07a
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-05-06 17:46:11 -0300

    add: devative queries

--- java-persistence/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ java-persistence/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -36,6 +36,8 @@ public class Principal {
                     1 - Buscar séries
                     2 - Buscar episódios
                     3 - Listar séries buscadas
+                    4 - Buscar série por titulo
+                    5 - Buscar série por ator
                     0 - Sair
                     """;
 
@@ -53,12 +55,42 @@ public class Principal {
                 case 3:
                     listarSeriesBuscadas();
                     break;
+                case 4:
+                    buscarSeriePorTitulo();
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
+                    break;
                 case 0:
                     System.out.println("Saindo...");
                     break;
                 default:
                     System.out.println("Opção inválida");
             }
+        }
+    }
+
+    private void buscarSeriesPorAtor() {
+        System.out.println("Qual o nome para busca:");
+        var nomeAtor = leitura.nextLine();
+        System.out.println("Avaliações a partir de que valor?");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = serieRepository.findByAtoresContainsIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
+        System.out.println("Séries em que "+nomeAtor+" trabalhou:");
+        seriesEncontradas.forEach(s ->
+                System.out.println(s.getTitulo() + " avaliação: "+ s.getAvaliacao()));
+    }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma série pelo nome:");
+        var nomeSerie = leitura.nextLine();
+
+        Optional<Serie> serieBuscada = serieRepository.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if(serieBuscada.isPresent()){
+            System.out.println("Dados da série: "+serieBuscada.get());
+        }else{
+            System.out.println("Série não encontrada");
         }
     }
 
@@ -90,9 +122,10 @@ public class Principal {
         System.out.println("Escolha uma série pelo nome:");
         var nomeSerie = leitura.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
+        Optional<Serie> serie = serieRepository.findByTituloContainingIgnoreCase(nomeSerie);
+//        Optional<Serie> serie = series.stream()
+//                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
+//                .findFirst();
 
         if (serie.isPresent()) {
             var serieEncontrada = serie.get();
--- java-persistence/screenmatch/src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ java-persistence/screenmatch/src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -3,5 +3,11 @@ package br.com.alura.screenmatch.repository;
 import br.com.alura.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+    List<Serie> findByAtoresContainsIgnoreCaseAndAvaliacaoGreaterThanEqual(String nomeAtor, Double avaliacao);
 }
