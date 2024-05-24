a009372570a388de7189f4adc095e55c652319b1
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-05-23 23:46:28 -0300

    wip: Construção das entidades de paciente e médico, validação dos dados e integração com bd

---
+++ java-api-rest/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,23 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paciente")
+public class PacienteController {
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    public void cadastrarPaciente(@RequestBody @Valid DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
+    }
+}
