da45954225d035c334f80164bf08f1b47e65247c
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-05-28 17:48:40 -0300

    wip: adićão da biblioteca auth0 para gerar o JWT

--- java-boas-praticas/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ java-boas-praticas/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -24,6 +24,6 @@ public class AutenticacaoController {
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok();
     }
 }
